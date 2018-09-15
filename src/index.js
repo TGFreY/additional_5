bad4c32140f0de8bc4624d30def89f753ef50d0a
TGFreY <t.g.frey19@gmail.com>
2018-09-15 23:19:12 +0300

    check sequences with stack from template

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    if (str.length % 2 !== 0) {
+        return false;
+    }
+
+    let stack = [];
+    let template = {};
+
+    for (let bracketsPair of bracketsConfig) {
+        template[bracketsPair[0]] = bracketsPair[1];
+    }
+
+    for (let bracket of str) {
+        let lastOpenBracket;
+
+        if (bracket in template) {
+            if (stack.length !== 0) {
+                lastOpenBracket = stack[stack.length - 1];
+                template[lastOpenBracket] == bracket ? (stack.pop()) : (stack.push(bracket));
+            } else {
+                stack.push(bracket);
+            }
+
+        } else {
+            lastOpenBracket = stack[stack.length - 1];
+            if (template[lastOpenBracket] == bracket) {
+                stack.pop();
+            }
+        }
+    }
+
+    if (stack.length > 0) {
+        return false;
+    }
+    return true;
+};
